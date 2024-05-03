4dc264e0838b6e6c19eedd3def609316dca3414c
suranga <surangakumarage@gmail.com>
2024-05-03 18:10:03 +0530

    initial

--- src/main/java/com/suranga/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/suranga/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,37 @@
 package com.suranga.inventoryservice;
 
+import com.suranga.inventoryservice.model.Inventory;
+import com.suranga.inventoryservice.repository.InventoryRepository;
+import com.suranga.inventoryservice.service.InventoryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository)
+	{
+		return args -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("book1");
+			inventory1.setQuantity(100);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("book2");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+
+
+		};
 	}
 
 }
