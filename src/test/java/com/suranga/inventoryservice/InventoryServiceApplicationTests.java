5d945064e2047c28550df8d6b32ef8aea10e9f5a
suranga <surangakumarage@gmail.com>
2024-05-03 13:13:39 +0530

    initial

---
+++ src/test/java/com/suranga/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suranga.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
